049f3027024e21e1ef0eaec66f92dc18788fa719
Ali Mihandoost <a.mihandoost@1dws.com>
2012-02-17 01:05:24 +0330

    change story

--- libs/php/controller.php
+++ libs/php/controller.php
@@ -9,32 +9,79 @@ initialize();
 
 $act = $_GET['act'] or doDie('Wrong action.');
 
+$track_validate = array(
+	'name' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::name' ),
+	'nickname' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::password' ),
+	'email' => array( 'filter' => FILTER_SANITIZE_EMAIL ),
+	'password' => array( 'filter' => FILTER_SANITIZE_STRING ), //TODO: research
+	'cellphone' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::tel' ),
+	'uuid' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
+	'device_name' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
+	'platform_name' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
+	'platform_version' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
+	'screen_width' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
+	'screen_height' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
+	'avail_width' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
+	'avail_height' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
+	'color_depth' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
+	'user_agent' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
+	'language' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
+	'track_code' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
+	'meta_name' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
+	'meta_content' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ) //TODO: test!
+);
+
 if($act=='track'){
 	
-	$track_validate = array(
-		'name' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::name' ),
-		'nickname' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::password' ),
-		'email' => array( 'filter' => FILTER_SANITIZE_EMAIL ),
-		'cellphone' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::tel' ),
-		//'user_id' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),	// user_id=0 by default bayad tavasote device send shavad!
-		'uuid' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-		'device_name' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-		'platform_name' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-		'platform_version' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-		'screen_width' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-		'screen_height' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-		'avail_width' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-		'avail_height' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-		'color_depth' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-		'user_agent' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-		'language' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-		'track_code' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-		'meta_name' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-		'meta_content' => array( 'filter' => FILTER_CALLBACK, 'options' => 'validate::test' ),
-	);
+	//$track_arr = escape_arr(filter_input_array(INPUT_GET,$track_validate));
 	
-	$track_arr = escape_arr(filter_input_array(INPUT_GET,$track_validate));
+	$track_arr = get_inputs($track_validate); // for each inpust check for err and doDie
 	
+	$app_id = $_SESSION['app_id'] = $_SESSION['app_id'] or get_app_id($track_arr['track_code']);
+	
+	$device_id = $_SESSION['device_id'] = $_SESSION['device_id'] or get_device_id($track_arr['uuid']); // insert device_id and user_id or update
+	
+	$user_id = $_SESSION['user_id'] = $_SESSION['user_id'] or get_user_id($device_id); // select only
+	
+	$client_ip = get_client_ip();
+	
+	if($track_arr['meta_name']=='login') user_login($track_arr['email'],$track_arr['password']);
+	if($track_arr['meta_name']=='register') user_register($track_arr['email'],$track_arr['password'],'...'); //TODO: ...
+	
+	db_insert($db['perfix'].'analytics',array(
+		'user_id' => $user_id,
+		//TODO: ...
+	));
+	
+	/*
+	TODO:
+	target: add2analytic
+	neeeds: user_id, app_id, device_id, client_ip
+	
+	check session if not:
+	app_id -> sellect track_code in apps table
+		if not fount then doDie
+	store in app_id session
+	
+	session!
+	device_id -> sellect uuid in device table
+		f: if not found -> insert it and assined id and insert quest user and assing user_id
+		t: if found -> update and fetch id and fetch user_id
+	
+	session!
+	user_id -> is ok !
+	
+	client_ip -> get from $_SERVER
+		check for proxy
+	
+	if meta_name=register -> update guest as user , login
+	if meta_name=login :
+		check if guest -> update usertable for change this quest's to real user (mullti device for 1 user) , login
+	
+	
+	insert into analytic !:D
+	
+	*/
 	if(	strlen($track_arr['uuid'])		>0	&&
 		strlen($track_arr['name'])		>0	&&
 		strlen($track_arr['password'])	>0	&&
